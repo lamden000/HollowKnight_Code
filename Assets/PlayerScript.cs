2d17bb53ed4d49ce18f2a0a33e7565a7b100c7b1
lamden000 <lamden150@gmail.com>
2024-09-15 14:45:22 +0700

    Initial commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        // Lấy input từ bàn phím
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // Tạo vector di chuyển
+        Vector2 move = new Vector2(moveX, moveY) * moveSpeed * Time.deltaTime;
+
+        // Cập nhật vị trí của nhân vật
+        transform.Translate(move);
+    }
+}
