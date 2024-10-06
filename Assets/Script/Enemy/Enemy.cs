a6a6f56f9c5b15ceaec8fb63436786c8b8f9e0e3
Vminhkiet <minhkietvo6@gmail.com>
2024-10-06 16:19:08 +0700

    add crawlid enemy

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,16 +7,38 @@ public interface IEnemy
     void Move();
     void Attack();
     void TakeDamage(int amount);
+    void SetState(int state);
+    void Render();
 }
 public abstract class EnemyBase : MonoBehaviour, IEnemy
 {
     protected int health;
     protected int attackPower;
+    protected int state;
 
+    protected float vx;
+    protected float vy;
+
+    protected int nx;
+    protected int ny;
+    protected Animator animator; // Tham chiếu đến Animator
+    protected SpriteRenderer spriteRenderer;
+    protected virtual void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>(); // Lấy component Animator
+    }
+    public virtual void SetState(int state){
+        this.state = state;
+    }
+    public int GetState()
+    {
+        return state;
+    }
     public abstract void Move();
 
     public abstract void Attack();
-
+    public abstract void Render();
     public virtual void TakeDamage(int amount)
     {
         health -= amount;
