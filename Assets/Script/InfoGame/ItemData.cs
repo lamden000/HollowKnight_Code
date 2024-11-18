42a39a30270bb11d30ac3d817f22613b9604f252
Vminhkiet <minhkietvo6@gmail.com>
2024-11-18 18:21:03 +0700

    Complete rain effect

---
+++ Assets/Script/InfoGame/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Game/Item")]
+public class ItemData : ScriptableObject
+{
+    public Sprite sprite;
+    public string Nameitem;
+}
